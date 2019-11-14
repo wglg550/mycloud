c85b6d656eb1bfd92a06f68748aa35095d99c384
wangliang <wg_lg520@21cn.com>
2019-11-14 17:36:46 +0800

    添加eureka认证服务

---
+++ cloud-eureka/src/main/java/com/cloud/eureka/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.cloud.eureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Description: eureka认证服务，开启认证必须添加此配置
+ * @Param:
+ * @return:
+ * @Author: wangliang
+ * @Date: 2019/11/14
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().httpBasic();
+        //http.httpBasic();
+    }
+}
